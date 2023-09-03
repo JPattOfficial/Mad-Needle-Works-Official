2dffee14d7fb59fbb1b7ed1cf39194207f00ea13
JPattOfficial <HeSpeaks7@gmail.com>
2023-09-03 12:54:17 -0700

    First Push

---
+++ MadNeedleWorksOfficialWebsite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MadNeedleWorksOfficialWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MadNeedleWorksOfficialWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MadNeedleWorksOfficialWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadNeedleWorksOfficialWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
