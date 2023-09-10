da40c49dc01d9be33842b5ccad65a5bf74a1d7bd
JPattOfficial <HeSpeaks7@gmail.com>
2023-09-09 18:50:38 -0700

    update

--- MadNeedleWorksOfficialWebsite/Controllers/HomeController.cs
+++ MadNeedleWorksOfficialWebsite/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace MadNeedleWorksOfficialWebsite.Controllers
             return View();
         }
 
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
