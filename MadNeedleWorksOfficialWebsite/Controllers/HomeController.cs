22852ea8809213eda102cd13ae935e2737534ddb
JPattOfficial <HeSpeaks7@gmail.com>
2023-09-09 23:24:11 -0700

    Added Products Page

--- MadNeedleWorksOfficialWebsite/Controllers/HomeController.cs
+++ MadNeedleWorksOfficialWebsite/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MadNeedleWorksOfficialWebsite.Controllers
             return View();
         }
 
+        public IActionResult Products()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
